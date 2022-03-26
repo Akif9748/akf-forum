c5c7f7c3b4431fbeef0d2ee509a7e4e01b50a89f
Akif9748 <akif9748@gmail.com>
2022-03-26 23:41:24 +0300

    Better admin button

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,2 +1,2 @@
 const admin = () => document.getElementById("admin").innerHTML =
-    '<div class="admin"><a class="admin" href="/admin"> <h3> You are admin, and you can go your page </h3></a></div><hr>';
+    '<div class="admin"><a class="admin" href="/admin"><p>You are admin, and you can go your page!</p></a></div><hr>';
