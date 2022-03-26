f5402363f941762256dc225fae779d98afb6c47a
Akif9748 <akif9748@gmail.com>
2022-03-26 23:28:10 +0300

    Added include to ejs files

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,2 +1,2 @@
 const admin = () => document.getElementById("admin").innerHTML =
-    '<a href="/admin"> <h3> You are admin, and you can go your page </h3></a><br>';
+    '<div class="admin"><a class="admin" href="/admin"> <h3> You are admin, and you can go your page </h3></a></div><hr>';
